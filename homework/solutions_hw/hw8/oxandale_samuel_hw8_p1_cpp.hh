72d3460b996933d94e950f1cd0c6d0dd104566cf
soxand16 <soxand16@aol.com>
2017-11-20 12:32:45 -0600

    makefile and headers

---
+++ homework/solutions_hw/hw8/oxandale_samuel_hw8_p1_cpp.hh
@@ -0,0 +1,3 @@
+// Interpolates between points contained in x and y at the location of x_new using
+// n points and using interpolation of order order
+double interpolate(double x_new, double *x, double *y, int n, int order);
